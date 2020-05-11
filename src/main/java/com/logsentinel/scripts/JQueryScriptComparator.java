53d4c17304cf6cf9939dc682281338f08d08ac18
Bozho <bozhidar.bozhanov@gmail.com>
2020-05-11 14:13:00 +0300

    parallel execution for faster results

--- src/main/java/com/logsentinel/scripts/JQueryScriptComparator.java
+++ src/main/java/com/logsentinel/scripts/JQueryScriptComparator.java
@@ -10,9 +10,17 @@ import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.net.ssl.SSLHandshakeException;
 
@@ -36,74 +44,102 @@ public class JQueryScriptComparator {
     
     public static void main(String[] args) throws Exception {
         System.setProperty("http.agent", "scriptinel.com");
+        System.setProperty("sun.net.client.defaultConnectTimeout", "7000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "15000");
+        
         String outPath = args.length > 1 ? args[1] : System.getProperty("java.io.tmpdir") + "/jquery.csv";
         
-        Map<String, String> hashes = new HashMap<>();
+        List<String> urls = new ArrayList<>();
         try (CSVParser parser = CSVFormat.DEFAULT.withSkipHeaderRecord().parse(
-                new InputStreamReader(new FileInputStream(new File(args[0])), StandardCharsets.UTF_8));
-                CSVPrinter out = new CSVPrinter(new FileWriter(outPath, true), CSVFormat.DEFAULT)) {
+                new InputStreamReader(new FileInputStream(new File(args[0])), StandardCharsets.UTF_8))) {
             for (CSVRecord record : parser) {
-                String url = record.get(0) + JQUREY_PATH;
-                try {
-                    for (String suffix : Arrays.asList("", MIN)) {
-                        String scriptUrl = url.replace(".js", suffix + ".js");
-                        String script = IOUtils.toString(new URL(scriptUrl), StandardCharsets.UTF_8);
-                        if (StringUtils.isBlank(script)) {
+                String url = record.get(0);
+                urls.add(url);
+            }
+        }
+        System.out.println("Working with " + urls.size() + " URLs");
+        
+        BlockingQueue<List<String>> queue = new LinkedBlockingQueue<>();
+        ExecutorService queueRunner = Executors.newSingleThreadExecutor();
+        queueRunner.execute(() -> {
+           try {
+               List<String> line;
+               try (CSVPrinter out = new CSVPrinter(new FileWriter(outPath, true), CSVFormat.DEFAULT)) {
+                   while ((line = queue.poll(60, TimeUnit.SECONDS)) != null) {
+                       try {
+                           out.printRecord(line);
+                           out.flush();
+                       } catch (Exception ex) {
+                           ex.printStackTrace();
+                       }
+                   }
+               }
+           } catch (Exception ex) {
+               ex.printStackTrace();
+           }
+        });
+        
+        Map<String, String> hashes = new HashMap<>();
+        urls.parallelStream().forEach(url -> {
+            try {
+                url = url + JQUREY_PATH;
+                for (String suffix : Arrays.asList("", MIN)) {
+                    String scriptUrl = url.replace(".js", suffix + ".js");
+                    String script = IOUtils.toString(new URL(scriptUrl), StandardCharsets.UTF_8);
+                    if (StringUtils.isBlank(script)) {
+                        continue;
+                    }
+                    
+                    String prefix = suffix.equals(MIN) ? JQUERY_MIN_VERSION_PREFIX : JQUREY_VERSION_PREFIX;
+                    int versionIdx = script.indexOf(prefix);
+                    if (versionIdx < 0) {
+                        // in many cases jquery.js is actually minified
+                        if (suffix.isBlank()) {
+                            versionIdx = script.indexOf(JQUERY_MIN_VERSION_PREFIX);
+                            if (versionIdx < 0) {
+                                continue;
+                            } else {
+                                suffix = MIN;
+                                prefix = JQUERY_MIN_VERSION_PREFIX;
+                            }
+                        } else {
                             continue;
                         }
-                        
-                        String prefix = suffix.equals(MIN) ? JQUERY_MIN_VERSION_PREFIX : JQUREY_VERSION_PREFIX;
-                        int versionIdx = script.indexOf(prefix);
-                        if (versionIdx < 0) {
-                            // in many cases jquery.js is actually minified
-                            if (suffix.isBlank()) {
-                                versionIdx = script.indexOf(JQUERY_MIN_VERSION_PREFIX);
-                                if (versionIdx < 0) {
-                                    continue;
-                                } else {
-                                    suffix = MIN;
-                                    prefix = JQUERY_MIN_VERSION_PREFIX;
-                                }
-                            } else {
-                                continue;
-                            }
-                        }
-                        
-                        int endIdx = suffix.equals(MIN) ? script.indexOf('|') : script.indexOf('\n', versionIdx);
-                        String version = script.substring(versionIdx + prefix.length(), endIdx).trim();
-                        boolean wpVersion = script.contains("| WordPress");
-                        String hash = hashes.get(version + (wpVersion ? "-wp" : ""));
-                        if (hash == null) {
-                            String gitHubScript;
-                            if (wpVersion) {
-                                gitHubScript = normalize(IOUtils.toString(new URL(GITHUB_WP_JQUERY_VERSION_URL), StandardCharsets.UTF_8));
-                            } else {
-                                gitHubScript = normalize(IOUtils.toString(new URL(GITHUB_ROOT + version + "/dist/jquery" + suffix + ".js"), StandardCharsets.UTF_8));
-                            }
-                            hash = DigestUtils.sha1Hex(gitHubScript); 
-                            hashes.put(version + (wpVersion ? "-wp" : ""), hash);
-                        }
-                        
-                        script = normalize(script);
-                        if (!DigestUtils.sha1Hex(script).equals(hash)) {
-                            out.printRecord(scriptUrl, "mismatch");
-                        } else {
-                            out.printRecord(scriptUrl, "ok");
-                        }
-                        out.flush();
                     }
-                } catch (UnknownHostException | ConnectException | SSLHandshakeException | FileNotFoundException | SocketTimeoutException ex) {
-                    // ignore
-                } catch (Exception ex) {
-                    if (ex.getMessage().startsWith("Server returned HTTP response code")) {
-                        // ignore
+                    
+                    int endIdx = suffix.equals(MIN) ? script.indexOf('|') : script.indexOf('\n', versionIdx);
+                    String version = script.substring(versionIdx + prefix.length(), endIdx).trim();
+                    boolean wpVersion = script.contains("| WordPress");
+                    String hash = hashes.get(version + (wpVersion ? "-wp" : ""));
+                    if (hash == null) {
+                        String gitHubScript;
+                        if (wpVersion) {
+                            gitHubScript = normalize(IOUtils.toString(new URL(GITHUB_WP_JQUERY_VERSION_URL), StandardCharsets.UTF_8));
+                        } else {
+                            gitHubScript = normalize(IOUtils.toString(new URL(GITHUB_ROOT + version + "/dist/jquery" + suffix + ".js"), StandardCharsets.UTF_8));
+                        }
+                        hash = DigestUtils.sha1Hex(gitHubScript); 
+                        hashes.put(version + (wpVersion ? "-wp" : ""), hash);
+                    }
+                    
+                    script = normalize(script);
+                    if (!DigestUtils.sha1Hex(script).equals(hash)) {
+                        queue.put(Arrays.asList(scriptUrl, "mismatch"));
                     } else {
-                        ex.printStackTrace();
+                        queue.put(Arrays.asList(scriptUrl, "ok"));
                     }
                 }
                 Thread.sleep(100);
+            } catch (UnknownHostException | ConnectException | SSLHandshakeException | FileNotFoundException | SocketTimeoutException ex) {
+                // ignore
+            } catch (Exception ex) {
+                if (ex.getMessage().startsWith("Server returned HTTP response code")) {
+                    // ignore
+                } else {
+                    ex.printStackTrace();
+                }
             }
-        }
+        });
     }
 
     private static String normalize(String script) {
--- src/main/java/com/logsentinel/scripts/WooCommerceScriptComparator.java
+++ src/main/java/com/logsentinel/scripts/WooCommerceScriptComparator.java
@@ -11,10 +11,17 @@ import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.net.ssl.SSLHandshakeException;
 
@@ -42,94 +49,120 @@ public class WooCommerceScriptComparator {
     public static void main(String[] args) throws Exception {
         
         System.setProperty("http.agent", "scriptinel.com");
+        System.setProperty("sun.net.client.defaultConnectTimeout", "7000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "15000");
+
         String outPath = args.length > 1 ? args[1] : System.getProperty("java.io.tmpdir") + "/wocommerce.csv";
         
         List<String> scripts = Arrays.asList("/assets/js/jquery-payment/jquery.payment.js", "/assets/js/frontend/woocommerce.js", 
                 "/assets/js/frontend/checkout.js", "/assets/js/frontend/cart.js",
                 "/assets/js/frontend/credit-card-form.js");
 
-        int problematicHosts = 0;
+        AtomicInteger problematicHosts = new AtomicInteger();
+        List<String> urls = new ArrayList<>();
         try (CSVParser parser = CSVFormat.DEFAULT.withSkipHeaderRecord().parse(
-                new InputStreamReader(new FileInputStream(new File(args[0])), StandardCharsets.UTF_8));
-                CSVPrinter out = new CSVPrinter(new FileWriter(outPath, true), CSVFormat.DEFAULT)) {
+                new InputStreamReader(new FileInputStream(new File(args[0])), StandardCharsets.UTF_8))) {
             for (CSVRecord record : parser) {
                 String url = record.get(0);
-                try {
-                    String home = IOUtils.toString(new URL(url), StandardCharsets.UTF_8);
-                    int versionIdx = home.indexOf(META_VERSION_PREFIX);
-                    if (versionIdx < 0) {
-                        continue;
-                    }
-                    String version = home.substring(versionIdx + META_VERSION_PREFIX.length(), 
-                            versionIdx + META_VERSION_PREFIX.length() + 6);
-                    if (version.endsWith("\"")) {
-                        version = version.substring(0, version.length() - 1);
-                    }
-                    for (String script : scripts) {
-                        for (String suffix : Arrays.asList("", ".min")) {
-                            String scriptName = script.replace(".js", suffix + ".js");
-                            String scriptUrl = GITHUB_ROOT + version + scriptName;
-                            String hash = hashes.get(version + scriptName); 
-                            if (hash == null) {
-                                try {
-                                    String githubScript = normalize(IOUtils.toString(new URL(scriptUrl), StandardCharsets.UTF_8));
-                                    if (StringUtils.isNotBlank(githubScript)) {
-                                        hash = DigestUtils.sha1Hex(githubScript);
-                                        hashes.put(version + scriptName, hash);
-                                        lengths.put(version + scriptName, githubScript.length());
-                                    }
-                                } catch (Exception ex) {
-                                    ex.printStackTrace();
+                urls.add(url);
+            }
+        }
+        System.out.println("Working with " + urls.size() + " URLs");
+        
+        BlockingQueue<List<String>> queue = new LinkedBlockingQueue<>();
+        ExecutorService queueRunner = Executors.newSingleThreadExecutor();
+        queueRunner.execute(() -> {
+           try {
+               List<String> line;
+               try (CSVPrinter out = new CSVPrinter(new FileWriter(outPath, true), CSVFormat.DEFAULT)) {
+                   while ((line = queue.poll(60, TimeUnit.SECONDS)) != null) {
+                       try {
+                           out.printRecord(line);
+                           out.flush();
+                       } catch (Exception ex) {
+                           ex.printStackTrace();
+                       }
+                   }
+               }
+           } catch (Exception ex) {
+               ex.printStackTrace();
+           }
+        });
+        
+        urls.parallelStream().forEach(url -> {
+            try {
+                String home = IOUtils.toString(new URL(url), StandardCharsets.UTF_8);
+                int versionIdx = home.indexOf(META_VERSION_PREFIX);
+                if (versionIdx < 0) {
+                    return;
+                }
+                String version = home.substring(versionIdx + META_VERSION_PREFIX.length(), 
+                        versionIdx + META_VERSION_PREFIX.length() + 6);
+                if (version.endsWith("\"")) {
+                    version = version.substring(0, version.length() - 1);
+                }
+                for (String script : scripts) {
+                    for (String suffix : Arrays.asList("", ".min")) {
+                        String scriptName = script.replace(".js", suffix + ".js");
+                        String scriptUrl = GITHUB_ROOT + version + scriptName;
+                        String hash = hashes.get(version + scriptName); 
+                        if (hash == null) {
+                            try {
+                                String githubScript = normalize(IOUtils.toString(new URL(scriptUrl), StandardCharsets.UTF_8));
+                                if (StringUtils.isNotBlank(githubScript)) {
+                                    hash = DigestUtils.sha1Hex(githubScript);
+                                    hashes.put(version + scriptName, hash);
+                                    lengths.put(version + scriptName, githubScript.length());
+                                }
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                        String siteScript = normalize(IOUtils.toString(new URL(url + WOOCOMMERCE_PREFIX + scriptName), 
+                                StandardCharsets.UTF_8));
+                        if (StringUtils.isBlank(siteScript) || siteScript.contains("</body>") || siteScript.contains("</html>")) {
+                            continue;
+                        }
+                        
+                        String siteHash = DigestUtils.sha1Hex(siteScript);
+                        
+                        if (hash != null && !siteHash.equals(hash)) {
+                            // check if the min.js is not actually expanded
+                            if (scriptName.endsWith(".min.js")) {
+                                siteScript = normalize(IOUtils.toString(new URL(url + WOOCOMMERCE_PREFIX + scriptName.replace(".min.js", ".js")), 
+                                        StandardCharsets.UTF_8));
+                                if (DigestUtils.sha1Hex(siteScript).equals(hashes.get(version + scriptName.replace(".min.js", ".js")))) {
+                                    queue.put(Arrays.asList(url + WOOCOMMERCE_PREFIX + scriptName, "ok"));
+                                    continue;
                                 }
                             }
-                            String siteScript = normalize(IOUtils.toString(new URL(url + WOOCOMMERCE_PREFIX + scriptName), 
-                                    StandardCharsets.UTF_8));
-                            if (StringUtils.isBlank(siteScript) || siteScript.contains("</body>") || siteScript.contains("</html>")) {
+                            
+                            if (Math.abs(siteScript.length() - lengths.get(version + scriptName)) < LENGTH_DIFF_THRESHOLD) {
+                                queue.put(Arrays.asList(url + WOOCOMMERCE_PREFIX + scriptName, "ok"));
                                 continue;
                             }
                             
-                            String siteHash = DigestUtils.sha1Hex(siteScript);
+                            System.out.println(normalize(IOUtils.toString(new URL(scriptUrl), StandardCharsets.UTF_8)));
+                            System.out.println(siteScript);
+                            System.out.println(url + WOOCOMMERCE_PREFIX + scriptName + " mismatch");
                             
-                            if (hash != null && !siteHash.equals(hash)) {
-                                // check if the min.js is not actually expanded
-                                if (scriptName.endsWith(".min.js")) {
-                                    siteScript = normalize(IOUtils.toString(new URL(url + WOOCOMMERCE_PREFIX + scriptName.replace(".min.js", ".js")), 
-                                            StandardCharsets.UTF_8));
-                                    if (DigestUtils.sha1Hex(siteScript).equals(hashes.get(version + scriptName.replace(".min.js", ".js")))) {
-                                        out.printRecord(url + WOOCOMMERCE_PREFIX + scriptName, "ok");
-                                        continue;
-                                    }
-                                }
-                                
-                                if (Math.abs(siteScript.length() - lengths.get(version + scriptName)) < LENGTH_DIFF_THRESHOLD) {
-                                    out.printRecord(url + WOOCOMMERCE_PREFIX + scriptName, "ok");
-                                    continue;
-                                }
-                                
-                                System.out.println(normalize(IOUtils.toString(new URL(scriptUrl), StandardCharsets.UTF_8)));
-                                System.out.println(siteScript);
-                                System.out.println(url + WOOCOMMERCE_PREFIX + scriptName + " mismatch");
-                                
-                                out.printRecord(url + WOOCOMMERCE_PREFIX + scriptName, "mismatch");
-                            } else {
-                                out.printRecord(url + WOOCOMMERCE_PREFIX + scriptName, "ok");
-                            }
-                            out.flush();
+                            queue.put(Arrays.asList(url + WOOCOMMERCE_PREFIX + scriptName, "mismatch"));
+                        } else {
+                            queue.put(Arrays.asList(url + WOOCOMMERCE_PREFIX + scriptName, "ok"));
                         }
-                    }
-                    
-                } catch (UnknownHostException | ConnectException | SSLHandshakeException | FileNotFoundException | SocketTimeoutException ex) {
-                    problematicHosts++;
-                } catch (IOException e) {
-                    if (e.getMessage().startsWith("Server returned HTTP response code")) {
-                        problematicHosts ++;
-                    } else {
-                        e.printStackTrace();
                     }
                 }
                 Thread.sleep(100);
+            } catch (UnknownHostException | ConnectException | SSLHandshakeException | FileNotFoundException | SocketTimeoutException ex) {
+                problematicHosts.incrementAndGet();
+            } catch (IOException | InterruptedException e) {
+                if (e.getMessage().startsWith("Server returned HTTP response code")) {
+                    problematicHosts.incrementAndGet();
+                } else {
+                    e.printStackTrace();
+                }
             }
-        }
+        });
         System.out.println("Problematic hosts: " + problematicHosts);
     }
 
